2cc1a1e0b23f50367eeea70388464e88eaecbd7d
Blueimpulse2517 <136184059+Blueimpulse2517@users.noreply.github.com>
2024-03-09 12:35:59 +0530

    backend commit

---
+++ index.js
@@ -0,0 +1,45 @@
+
+const cors = require("cors")
+const express = require("express");
+const app = express();
+const StudentProfileRoutes = require("./Routes/StudentProfileRoutes");
+const EmpProfileRoutes = require("./Routes/EmpProfileRoutes");
+const jobpostRoutes = require("./Routes/JobpostsRoutes");
+const adminRoutes =require("./Routes/AdminRout")
+const PaymentRoute = require("./Routes/PaymentRout")
+//require("dotenv").config
+const { MongoClient } = require("mongodb")
+const mongoose = require("mongoose");
+//const port = process.env.PORT
+const port = 8080
+//mongoose.connect(process.env.MONGO_URL)
+mongoose.connect("mongodb+srv://blueimpluse:jobportal1234@cluster0.5dgcnm4.mongodb.net/jobportalMern")
+//mongoose.connect("mongodb://127.0.0.1:27017/Job-Portal-Database")
+
+    .then((res) => { console.log("connected") })
+    .catch(() => { console.log("failed") })
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('public'))
+
+
+
+
+app.use("/StudentProfile",StudentProfileRoutes)
+app.use("/EmpProfile",EmpProfileRoutes)
+app.use("/jobpost", jobpostRoutes)
+app.use("/admin", adminRoutes)
+app.use("/paymentAPI", PaymentRoute)
+
+
+
+app.use("*", (req, res) => {
+    res.send(" Cannot get from backend with **")
+})
+
+
+
+app.listen(port, () => {
+    console.log(`app running on port ${port} for booking`)
+})
