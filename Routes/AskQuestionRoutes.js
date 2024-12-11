7bded005db2cc44ea33f4f2dbf2ee24f19880c59
parvaiz1 <97357523+parvaiz1@users.noreply.github.com>
2024-12-12 01:06:29 +0530

    commit

---
+++ Routes/AskQuestionRoutes.js
@@ -0,0 +1,99 @@
+
+const express = require("express")
+const router = express.Router()
+const JobpostsModel = require("../Schema/PostJobSchema")
+const CareerJobpostsModel = require("../Schema/CareerJobSchema")
+const JobAppliedModel = require("../Schema/JobAppliedSchema")
+const StudentProfileModel= require("../Schema/StudentProfileSchema")
+const Archived= require("../Schema/ArchiveAchema")
+const Deleted= require("../Schema/DeletedJobsSchema")
+var nodemailer = require('nodemailer');
+const AskQuestionModel=require("../Schema/AskQuesionsSchema")
+
+const { MongoClient } = require("mongodb")
+// const {getData} = require("../mongodb")
+const {ObjectID} = require("mongodb")
+const {gtoken} = require('./EmpProfileRoutes')
+const secretKey = "abcde"
+const jwt = require("jsonwebtoken")
+
+const BlogModel=require("../Schema/PostBlogsSchema")
+
+
+// Middleware
+function verifyToken(req, res, next){
+    if(req.headers['authorization']){
+    let token = req.headers['authorization'].split(" ")[1]
+    let id = req.headers['authorization'].split(" ")[0]
+    if(token){
+        jwt.verify(token, secretKey, (err, valid)=>{
+    if(err){
+        res.send("invalid token")
+        }else{
+    let validid=valid.id
+    if(validid===id){
+        next()
+    }
+        }   })
+    }else{
+        res.send("Unauthorised Access")
+    }
+}
+}
+// middleware for 
+function verifyHomeJobs(req, res, next){
+    let valid=req.headers['authorization']
+    if(valid==='BlueItImpulseWalkinIn'){
+        next()
+}else{
+    res.send("Unauthorised Access")
+}
+}
+
+// employee Blog  postings
+router.post("/questionPost", verifyToken, async (req, res) => {
+    try {
+            let jobs = new AskQuestionModel(req.body)
+            let result = await jobs.save()
+            res.send("success")        
+    } catch (error) {
+        // console.log(error.message)
+        res.send("server issue ")
+    }
+})
+
+// ............get all Home jobs for all......
+router.get("/getAllBlogs",  async (req, res) => {
+    try {
+        let jobs = await BlogModel.find().select()
+        res.send(jobs)
+    } catch (err) {
+        res.status(401).send("server issue")
+    }
+})
+
+//  get job by Tag filter
+router.get("/getTagsJobs/:name", async(req, res)=>{
+    try{
+        let result = await BlogModel.aggregate([{$match:{Tags:req.params.name}}]) 
+        //or
+    // let result = await JobpostsModel.find({Tags:  req.params.name })
+    // let result = await JobpostsModel.find({Tags: {$elemMatch: {value: req.params.name }}}) //this one if for object in array in db
+        res.send(result)
+    }catch(err){
+        res.send("server error")
+    }
+})
+
+// .........getJobs for job details...........
+router.get("/getjobs/:id",verifyHomeJobs, async (req, res) => {
+    try {
+        let jobs = await BlogModel.findOne({ _id: req.params.id })
+        res.send(jobs)
+    } catch (err) {
+        res.status(401).send("server issue")
+    }
+})
+
+
+module.exports=router
---
+++ Schema/AskQuesionsSchema.js
@@ -0,0 +1,84 @@
+const mongoose = require("mongoose")
+
+const productSchema = new mongoose.Schema({
+    Logo:{
+    type :String
+
+    },
+jobTitle:{
+    type :String
+},
+companyName:{
+    type : String
+},
+name:{
+    type : String
+},
+SourceLink:{
+    type : String
+},
+Source:{
+    type : String
+},
+SourceCompanyLink:{
+    type : String
+},
+Adminpost:{
+    type:Boolean
+},
+empId:{
+type : String
+},
+adminLogin:{
+    type : String
+},
+jobDescription:{
+    type : String
+},
+jobtype:{
+    type : String
+},
+salaryRange:{
+    type : String
+},
+Tags:[
+    
+],
+jobLocation:{
+    type : String
+},
+qualification:{
+    type : String
+},
+experiance:{
+    type : String
+},
+skills:[
+    // type : String
+ ] ,
+jobSeekerId:[
+    {
+        jobSeekerId:{
+    type : String
+        },
+        date:{
+        type: Date
+    }
+    },
+    {
+}
+    
+ ] ,
+slectedJobseker:[
+],
+rejectedJobseker:[
+],
+onHoldJobseker:[
+]
+
+},
+
+{timestamps:true}
+)
+const productModel = mongoose.model("AskQuestions" , productSchema)
+module.exports = productModel
