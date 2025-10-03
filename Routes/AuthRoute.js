e3fbc40275b7b755aa55d09b8fed3e0e532e74dd
Blueimpulse2517 <blueimpulse2517@gmail.com>
2025-10-03 18:46:51 +0530

    QGT backend APIs

--- Routes/AuthRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { linkedInCallback, getUser } = require('../Authcontroller');
-
-const AuthRoutes = express.Router();
-
-AuthRoutes.get('/callback', linkedInCallback);
-AuthRoutes.get('/get-user', getUser);
-
-module.exports = AuthRoutes;
